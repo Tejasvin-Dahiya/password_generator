528088abe0bc94ecaf5d765b9278049d884b0782
Tejasvin Dahiya <tejasvindahiya@Tejasvins-MacBook-Air.local>
2025-03-06 15:11:05 +0530

    made changes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Password Generator Project
+#Random Password Generator Project
 import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -13,8 +13,7 @@ for i in range(1,nr_letters+1):
   password_list+= random.choice(letters)
   
 for z in range(1,nr_symbols+1):
-  password_list+=random.choice(symbols)
-  
+  password_list+=random.choice(symbols)  
 for n in range(1,nr_numbers+1):
   password_list+=random.choice(numbers)
 random.shuffle(password_list)
